e0c47c3b8edafafe8fb0aba2389c0a3fc44ec4f9
REZA <reza.karami.arbeiten@gmail.com>
2025-03-13 00:33:36 +0100

    Update FastAPI to serve web UI

--- app/main.py
+++ app/main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from .routs import router
 
 app = FastAPI()
+
+# Mount the static folder to serve HTML
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 # Include API routes
 app.include_router(router)
 
 @app.get("/")
 def home():
-    return {"message": "Medical Device Recommender API is running!"}
+    """Serves the HTML frontend."""
+    return {"message": "Go to /static/index.html to use the web interface"}
